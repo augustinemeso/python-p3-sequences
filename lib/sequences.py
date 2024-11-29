119395f930d5d59c1765c72948ff0f4f55cd9b54
augustinemeso <augustinemesobk@gmail.com>
2024-11-29 17:12:12 +0300

    add updated PYTHON-P3-SEQUENCES

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
-#!/usr/bin/env python3
+# sequences.py
 
 def print_fibonacci(length):
-    pass
+    fib_sequence = []
+    
+    if length > 0:
+        fib_sequence.append(0)
+    if length > 1:
+        fib_sequence.append(1)
+    
+    for i in range(2, length):
+        fib_sequence.append(fib_sequence[i - 1] + fib_sequence[i - 2])
+    
+    print(fib_sequence)
+
+# Test the function by calling it with a sample length
+print_fibonacci(10)
